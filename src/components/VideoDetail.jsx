f827c0ed007065df9901ecc28d85c5102b47c384
unknown <kalilulekan@gmail.com>
2023-06-28 00:59:53 +0100

    player

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -9,19 +9,52 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
     const {id} = useParams();
-    const [VideoDetail, setVideoDetail]= useState(null)
+    const [videos ,setVideos] = useState(null)
+    const [videoDetail, setVideoDetail]= useState(null)
     useEffect(()=>{
-        fetchFromAPI(`videos?part=snippet.statistics&id={id}`)
-        .then((data)=> setVideoDetail(data.item[0]))
+        fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+        .then((data)=> setVideoDetail(data.items[0]))
+
+        fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}`)
+        .then((data) => setVideos(data.items))
      },[id] )
+
+     if (videoDetail === null){
+        return <h1>loading .... </h1>
+     }
+
+     const {snippet:{title, channelId, channelTitle}, statistics:{viewCount, likeCount}}= videoDetail
     return(
         <Box minHeight='95vh'>
             <Stack direction={{ xs: 'column' , md:'row'}}>
                 <Box flex={1}>
                     <Box sx={{ width: '100%' , position: 'sticky', top: '86px' }}>
-                        <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className='react-player' controls />
+                        <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}
+                         className='react-player' controls />
+                         <Typography color='#fff' variant='h5' fontWeight='bold' p={2}>
+                            {title}
+                         </Typography>
+                         <Stack>
+                            <Link to={`/channel/${channelId}`}>
+                            <Typography variant={{ sm:'subtitle1', md: 'h6'}}
+                            color='#fff'>{channelTitle}
+                            <CheckCircle  sx={{ fontsize: '12px', color: 'gray', ml:'5px'}}/>
+                            </Typography>
+                            </Link>
+                            <Stack direction='row' gap='20px' alignItems='center'>
+                                <Typography variant='body1' sx={{ opacity: 0.7}}>
+                                    {parseInt(viewCount).toLocaleString()} views
+                                </Typography>
+                                <Typography variant='body1' sx={{ opacity: 0.7}}>
+                                    {parseInt(likeCount).toLocaleString()}
+                                </Typography>
+                            </Stack>
+                         </Stack>
                     </Box>
                 </Box>
+                <Box px={2} py={{md: 1, xs: 5}} justifyContent='center' alignItems='center'>
+                <Videos videos={videos} direction='column'/>
+            </Box>
             </Stack>
         </Box>
     )}
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import {Stack, Box } from '@mui/material'
 import {VideoCard, ChannelCard} from './'
 
-const Videos = ({videos}) => {
+const Videos = ({videos, direction}) => {
+  if(!videos?.length)return 'loading'
  return(
-    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
+    <Stack direction={direction || 'row'} flexWrap='wrap' justifyContent='start' gap={2}>
 
       {videos.map((item, idx)=>(
         <Box key={idx}>
